ab512ad5da6834eeffeaeda743a5f787521a7f4c
AyariSiwar <91055926+Siwarayari@users.noreply.github.com>
2023-12-11 13:26:23 +0100

    basketballshots

--- 0x01-dart-function/7-basketball-shots.dart
+++ 0x01-dart-function/7-basketball-shots.dart
@@ -1,41 +1,21 @@
-int whoWins(Map<String, int> teamA, Map<String, int> teamB){
-    int scoreofteam1 = 0;
-    int scoreofteam2 = 0;
+int whoWins(Map<String, int> teamA, Map<String, int> teamB) {
+  int teamAtotal = teamA['Free throws']! + teamA['2 pointers']! * 2 + teamA['3 pointers']! * 3;
+  int teamBtotal = teamB['Free throws']! + teamB['2 pointers']! * 2 + teamB['3 pointers']! * 3;
+  
+  if (teamAtotal > teamBtotal) {
+    return 1;
+  }
+  else if (teamAtotal < teamBtotal) {
+    return 2;
+  }
+  else {
+    return 0;
+  }
+}
 
-    teamA.forEach((type, result){
-        if (type == 'Free throws'){
-            scoreofteam1 += result * 1;
-        }
-        else if (type == '2 pointers'){
-            scoreofteam1 += result * 2;
-        }
-        else if (type == '3 pointers'){
-            scoreofteam1 += result * 2;
-        }
-    });
+main() {
+  var teamA = {'Free throws' : 23, '2 pointer' : 32, '3 pointer': 16};
+  var teamB = {'Free throws' : 31, '2 pointer' : 27, '3 pointer': 13};
 
-
-    teamB.forEach((type, result){
-        if (type == 'Free throws'){
-            scoreofteam2 += result * 1;
-        }
-        else if (type == '2 pointers'){
-            scoreofteam2 += result * 2;
-        }
-        else if (type == '3 pointers'){
-            scoreofteam2 += result * 2;
-        }
-    });
-
-
-
-    if (scoreofteam1 == scoreofteam2){
-        return 0;
-    }
-    else if(scoreofteam1 < scoreofteam2) { 
-        return 2;
-    }
-    else{
-        return 1;
-    }
+  print(whoWins(teamA, teamB));
 }
